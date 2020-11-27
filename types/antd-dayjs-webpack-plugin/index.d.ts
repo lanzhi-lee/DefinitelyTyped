4ac73e6e426d3699ca024a432d10f725b2f57732
lanzhi_lee <baibai_lee@163.com>
2020-11-27 20:39:16 +0800

    Added type definitions for antd-dayjs-webpack-plugin

---
+++ types/antd-dayjs-webpack-plugin/index.d.ts
@@ -0,0 +1,33 @@
+// Type definitions for antd-dayjs-webpack-plugin
+// Project: https://github.com/ant-design/antd-dayjs-webpack-plugin
+// Definitions by: Lanzhi <https://github.com/lanzhi-lee>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Compiler } from 'webpack'
+
+/**
+ * A webpack plugin for Day.js.
+ * Replace Moment.js with Day.js in antd project in ONE step. Bundle size reduced from 65 kb -> 4.19 kb.
+ * Configuration Detail: https://github.com/ant-design/antd-dayjs-webpack-plugin
+ */
+declare class AntdDayjsWebpackPlugin {
+  constructor(options?: AntdDayjsWebpackPlugin.PluginOptions)
+
+  /**
+   * Apply the plugin
+   */
+  apply(compiler: Compiler): void
+}
+
+declare namespace AntdDayjsWebpackPlugin {
+  interface PluginOptions {
+    /**
+     * By default, the preset is 'antd'
+     */
+    preset?: 'antd' | 'antdv3'
+    plugins?: string[]
+    replaceMoment?: boolean
+  }
+}
+
+export = AntdDayjsWebpackPlugin
